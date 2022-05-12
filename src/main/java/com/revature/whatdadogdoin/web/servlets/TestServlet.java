95610655d2808d928a0923c5e32ae4bccf91c302
Matthew Swaney <Matthew.J.Swaney@gmail.com>
2022-05-12 16:46:36 -0500

    update pom.xml, added web.xml, added testservlet

---
+++ src/main/java/com/revature/whatdadogdoin/web/servlets/TestServlet.java
@@ -0,0 +1,16 @@
+package com.revature.whatdadogdoin.web.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.getWriter().write("<h1>Test Servlet is working as intended for What-Da-Dog-Doin'</h1>");
+
+    }
+
+}
